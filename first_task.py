515f542251511bbdfdb35e9ecef5a43e8638f49d
ravinaNG <ravina18@navgurukul.org>
2019-07-17 23:27:13 +0530

    web-scraping

--- first_task.py
+++ first_task.py
@@ -1,28 +1,35 @@
 import requests
 import json
 import html5lib
+from pprint import pprint
 from bs4 import BeautifulSoup
 movie_link =  "https://www.imdb.com/india/top-rated-indian-movies/"
 data = requests.get(movie_link)
 movies_data = data.content
 soup = BeautifulSoup(movies_data, 'html5lib')
 tbody = soup.find('tbody',{'class':'lister-list'})
-for tr in tbody.findAll('tr'):
-    td = (tr.find_all("td"))
-    # print (td)
+trs = tbody.find_all('tr')
+def scrape_top_list():
+    dic_list = []
     i = 0
-    j = 0
-    demo = []
-    while (i<len(td)):
-        data = td[i].get_text()
-
-        if j == 1 or j == 2:
-                meta = data.split()
-                demo.extend(meta)
-        j +=1
+    for tr in trs: 
+        dic = {}
         i = i + 1
-    print(demo)
-# just chacking
+        name = tr.find('td', class_= 'titleColumn').a.get_text()
+        year = tr.find('td', class_= 'titleColumn').span.get_text()
+        url_movies = tr.find('td', class_='titleColumn').a['href']
+        link =  'https://www.imdb.com' + url_movies
+        rate = tr.find('td', class_= 'ratingColumn imdbRating').strong.get_text()
+        dic['year']= int(year[1:5])
+        dic['rating'] = float(rate)
+        dic['POSTION'] = int(i)
+        dic['name'] = str(name)
+        dic['url']= str(link)
+        # print (dic)
+        dic_list.append(dic)
+    return dic_list
+top_movies = (scrape_top_list()) 
+pprint (top_movies)
 
 
 
@@ -33,16 +40,36 @@ for tr in tbody.findAll('tr'):
 
 
 
-# print (tr.get_text())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # span = tbody.findAll('span')
-# # for name in tr:
-# #     print (name.get_text())
-# # for year in span:
-# #     print (year.get_text())
+# tr = tbody.find_all('tr')
 # rating = tbody.findAll('strong')
-# # for rate in rating:
-# #     print (rate.get_text())
-# img = tbody.find('a')
-# print (img.get)
-# # for url in img:
-#     print (url.get_text())
+# img = tbody.findAll('a')
+# for url in img:
+#     print (url.get_text()) # Names of movies
+# for name in tr:
+#     print (name.get_text())  # in tr everything is there like name, year, rating, position.
+# for year in span:
+#     print (year.get_text())  # year of the movies 
+# for rate in rating:
+#     print (rate.get_text()) # rating of movies
