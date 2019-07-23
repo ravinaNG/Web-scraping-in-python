4d54751c343348efe009a9550acd2a37b6876bb4
ravinaNG <ravina18@navgurukul.org>
2019-07-23 19:34:29 +0530

    First task done :)

--- first_task.py
+++ first_task.py
@@ -12,29 +12,34 @@ tbody = soup.find('tbody', {'class':'lister-list'})
 trs = tbody.findAll('tr')
 # print (type(trs)) # 'bs4.element.ResultSet' this is the type of trs but it is same like list.
 # print (len(trs))
-movies_data = []
-td = 1
-while (td <= len(trs)):
-    dic = {}
-    if(td == len(trs)):
-        break
-    name = trs[td].find('td', class_ = 'titleColumn').a.get_text()
-    year = trs[td].find('td', class_ = 'titleColumn').span.get_text()
-    demo = int(year.replace("(", "").replace(")", ""))
-    # yea = list(year)
-    # year = (yea.remove(yea[0]))
-    # year = yea
-    # yea = (year.remove(year[4]))
-    # yea = year
-    # year = ''.join(string for string in yea)
-    # print(year)
-    rating = trs[td].find('td', class_ = 'ratingColumn imdbRating').strong.get_text()
-    url = trs[td].find('td', class_= 'posterColumn').a['href'] # her we don't use get_text() because the link is the value of a['href']
-    movie_link = "https://www.imdb.com/" + url
-    dic['name'] = name
-    dic['year'] = demo
-    dic['rating'] = rating
-    dic['url'] = movie_link
-    dic['position'] = td
-    pprint (dic)
-    td = td + 1
+def scrap_top_list(trs):
+    movies_data = []
+    td = 1
+    while (td <= len(trs)):
+        dic = {}
+        if(td == len(trs)):
+            break
+        name = trs[td].find('td', class_ = 'titleColumn').a.get_text()
+        year = trs[td].find('td', class_ = 'titleColumn').span.get_text()
+        demo = int(year.replace("(", "").replace(")", ""))
+        # yea = list(year)
+        # year = (yea.remove(yea[0]))
+        # year = yea
+        # yea = (year.remove(year[4]))
+        # yea = year
+        # year = ''.join(string for string in yea)
+        # print(year)
+        rating = trs[td].find('td', class_ = 'ratingColumn imdbRating').strong.get_text()
+        url = trs[td].find('td', class_= 'posterColumn').a['href'] # her we don't use get_text() because the link is the value of a['href']
+        movie_link = "https://www.imdb.com/" + url
+        dic['name'] = name
+        dic['year'] = demo
+        dic['rating'] = rating
+        dic['url'] = movie_link
+        dic['position'] = td
+        pprint (dic)
+        movies_data.append(dic) 
+        td = td + 1
+    return movies_data
+
+pprint (scrap_top_list(trs))
