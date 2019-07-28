96ce89216ec8d07a6ae5ada57d910d884e1942f7
ravinaNG <ravina18@navgurukul.org>
2019-07-28 11:33:22 +0530

    Web - Scraping

--- third_task.py
+++ third_task.py
@@ -5,18 +5,19 @@ end = 1950
 dict_of_list = {}
 index = 0
 current_decade = 2020
-for index in movie_data:
+while (index < len(movie_data)):
     if(end == current_decade):
         break
     start = end
     end = start + 10
     years_list = []
-    for year in movie_data :
-        if(year['year'] >= start and year['year'] < end):
-            years_list.append(index)
-        # year = year + 1
+    year = 0
+    while (year < len(movie_data)):
+        if(movie_data[year]['year'] >= start and movie_data[year]['year'] < end):
+            years_list.append(movie_data[year])
+        year = year + 1
     dict_of_list[start] = years_list
-    # index = index + 1
+    index = index + 1
 pprint (dict_of_list)
 
 
@@ -26,17 +27,26 @@ pprint (dict_of_list)
 
 
 
-# while (index < len(movie_data)):
+
+
+
+
+
+
+
+
+
+
+# for index in movie_data:
 #     if(end == current_decade):
 #         break
 #     start = end
 #     end = start + 10
 #     years_list = []
-#     year = 0
-#     while (year < len(movie_data)):
-#         if(movie_data[year]['year'] >= start and movie_data[year]['year'] < end):
-#             years_list.append(movie_data[year])
-#         year = year + 1
+#     for year in movie_data :
+#         if(year['year'] >= start and year['year'] < end):
+#             years_list.append(index)
+#         # year = year + 1
 #     dict_of_list[start] = years_list
-#     index = index + 1
+#     # index = index + 1
 # pprint (dict_of_list)
--- third_task2.py
+++ third_task2.py
@@ -1,49 +1,48 @@
 from first_task import data_in_formate as movie_data
 from pprint import pprint
-dicade_dict = {}
-module = movie_data[0]['year'] % 10
-start_year = movie_data[0]['year'] - module
-end_year = movie_data[0]['year'] + 9
-for index in (movie_data):
-    dict_list = []
-    for year in movie_data:
-        if(year['year']< end_year and year['year'] >= start_year):
-            dict_list.append(year)
-    dicade_dict[start_year]=dict_list
-    start_year = end_year+1
-    end_year = start_year+9  
-pprint (dicade_dict) 
+# pprint (movie_data)
+def yearsList(data):
+    yearList = []
+    for year in data:
+        yearList.append(year['year'])
+    return yearList
 
+def maxYear(yearList):
+    maxNum = yearList[0]
+    for index in range(len(yearList)):
+        if(yearList[index] > maxNum):
+            maxNum = yearList[index]
+    return (maxNum)
 
+def miniYear(yearList):
+    miniNum = yearList[0]
+    for index in range(len(yearList)):
+        if(yearList[index] < miniNum):
+            miniNum = yearList[index]
+    return (miniNum)
 
+def group_by_decade(data, maxYear, miniYear):
+    dicade_dict = {}
+    lastDigitOfYear = miniYear%10
+    maximumYear = miniYear - lastDigitOfYear - 1
+    for miniYear in range(maxYear):
+        if(maximumYear >= maxYear):
+            return dicade_dict
+        dict_list = []
+        minimumYear = maximumYear + 1
+        maximumYear = minimumYear + 9
+        for year in movie_data:
+            if(year['year']< maximumYear and year['year'] >= minimumYear):
+                dict_list.append(year)
+        dicade_dict[minimumYear]=dict_list
 
+yearsList = yearsList(movie_data)
+maxYear = maxYear(yearsList)
+miniYear = miniYear(yearsList)
+decadeDict = group_by_decade(movie_data, maxYear, miniYear)
+print (maxYear, end = ":-  Maximum year")
+print (" ")
+print (miniYear, end = ":-  Minimum year")
+print (" ")
+pprint (decadeDict)
 
-
-
-
-
-
-
-
-
-# from first_task import data_in_formate as movie_data
-# from pprint import pprint
-# dicade_dict = {}
-# length = len(movie_data)-1
-# last_year = movie_data[length]['year']
-# module = movie_data[0]['year'] % 10
-# start_year = movie_data[0]['year'] - module
-# end_year = movie_data[0]['year'] + 9
-# index = start_year
-# for index in range(start_year, last_year):
-#     print (index)
-#     if(start_year >= movie_data[length]['year']):
-#         break
-#     dict_list = []
-#     for year in movie_data:
-#         if(year['year']< end_year and year['year'] >= start_year):
-#             dict_list.append(year)
-#     dicade_dict[start_year]=dict_list
-#     start_year = end_year+1
-#     end_year = start_year+9  
-# pprint (dicade_dict) 
