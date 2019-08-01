98f39bc01978e2ae5cc906abb9773fb3dbcb7526
ravinaNG <ravina18@navgurukul.org>
2019-08-02 00:04:21 +0530

    Web-scraping

--- first_task.py
+++ first_task.py
@@ -1,43 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
-import re
 # import html5lib
 movies_link = "https://www.imdb.com/india/top-rated-indian-movies/"
 movies_data = requests.get(movies_link).content
-# pprint (movies_data)
 soup = BeautifulSoup(movies_data, 'html5lib')
-# # print ("******************************************************")
-# # print ("")
-# print (soup)
-
 tbody = soup.find('tbody', {'class':'lister-list'})
-# # print (tbody)
 trs = tbody.findAll('tr')
 # # print (type(trs)) # 'bs4.element.ResultSet' this is the type of trs but it is same like list.
-# print (len(trs))
 def scrap_top_list(trs):
     movies_data = []
-    td = 1
+    td = 0
     while (td <= len(trs)):
         dic = {}
         if(td == len(trs)):
             break
         name = trs[td].find('td', class_ = 'titleColumn').a.get_text()
         year = trs[td].find('td', class_ = 'titleColumn').span.get_text()
-        demo = int(year.replace("(", "").replace(")", ""))
-        # yea = list(year)
-        # year = (yea.remove(yea[0]))
-        # year = yea
-        # yea = (year.remove(year[4]))
-        # yea = year
+        movie_rlg_year = int(year.replace("(", "").replace(")", ""))
         # year = ''.join(string for string in yea)
-        # print(year)
         rating = trs[td].find('td', class_ = 'ratingColumn imdbRating').strong.get_text()
         url = trs[td].find('td', class_= 'posterColumn').a['href'] # her we don't use get_text() because the link is the value of a['href']
-        movie_link = "https://www.imdb.com/" + url
+        movie_link = "https://www.imdb.com" + url
         dic['name'] = name
-        dic['year'] = demo
+        dic['year'] = movie_rlg_year
         dic['rating'] = rating
         dic['url'] = movie_link
         dic['position'] = td
---
+++ fourth_task.py
@@ -0,0 +1,77 @@
+from first_task import data_in_formate as moviesData
+from bs4 import BeautifulSoup
+# import html5lib
+from pprint import pprint 
+import requests
+
+def storeMoviesUrl(movieData):
+    urlList = []
+    for index in movieData:
+        urlList.append(index['url'])
+    return urlList
+
+def movieName(list):
+    index = 0
+    movie_name = ""
+    while (index < len(list)):
+        movie_name = movie_name + list[index]
+        index = index + 1
+    return movie_name
+
+def scrape_movie_details(movieUrl):
+    list1 = []
+    detailsss = {}
+    getData = requests.get(movieUrl).content
+    soup = BeautifulSoup(getData, 'html5lib')
+    div = soup.find('div', {'class':'titleBar'})
+    find_name = div.find('div', {'class':'title_wrapper'})
+    name_year = find_name.h1.get_text()
+    demo = name_year.split()
+    remove_year = demo.pop()
+    movie_name = (movieName(demo)) # movie name
+
+    details = soup.find('div', {'class':'plot_summary'})
+    director = details.a.get_text() 
+    director_name = []
+    director_name.append(director) # Derector name
+
+    text = details.find('div', {'class':'summary_text'}).get_text()
+    movie_text = text.strip() # movie text
+
+    urls = soup.find('div', {'class':'slate_wrapper'})
+    image_url = urls.find('div', {'class':'slate'}).a.img['src'] # poster image url
+
+    country_class = soup.find('div', {'class':'article', 'id':'titleDetails'})
+    txt_blocks = country_class.findAll('div', {'class':'txt-block'})
+    for name in txt_blocks: 
+        if 'Country' in name.text:
+            country = name
+        elif 'Language' in name.text:
+            lang = name
+    country_name = country.a.get_text() # country name
+    language = lang.a.get_text() # language
+    languages = []
+    languages.append(language)
+
+    runTime = find_name.find('div', {'class':'subtext'}).time.get_text()
+    runtime = runTime.strip() # Runtime
+
+    genre = find_name.find('div', {'class':'subtext'}).a.get_text() # Genre
+    genres = []
+    genres.append(genre)
+
+    # whole details in dictionary formate. :)
+    detailsss['name'] = movie_name
+    detailsss['director'] = director_name
+    detailsss['country'] = country_name
+    detailsss['language'] = languages
+    detailsss['poster_image_url'] = image_url
+    detailsss['bio'] = movie_text
+    detailsss['runtime'] = runtime
+    detailsss['genre'] = genres
+    pprint(detailsss)
+
+urlList = storeMoviesUrl(moviesData)
+url = urlList[0]
+# print (url)
+scrape_movie_details(url)
