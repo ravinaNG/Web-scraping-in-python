376bb6ba0c35c200a34a0d63b0d712d769f0373a
ravinaNG <ravina18@navgurukul.org>
2019-08-11 10:49:50 +0530

    web scraping

--- Sauces n pickles (paytm)/picklesAndSauces.py
+++ Sauces n pickles (paytm)/picklesAndSauces.py
@@ -9,31 +9,36 @@ def topDetailsOfPickles(url):
     soup = BeautifulSoup(data, 'html5lib')
     _2mYo = soup.find('div', {'class':'_2mYo'})
     _3RA = _2mYo.find('div', {'class':'_3RA-'})
-    _1fje = _3RA.findAll('div', {'class':'_1fje'})
-
+    column = _3RA.findAll('div', {'class':'_1fje'})
+    num = 1
     AlldetailsList = []
-    for index in _1fje:
-        OnedetailsDict = {}
-        _2i1r = index.find('div', {'class':'_2i1r'})
-        _3WhJ = _2i1r.find('div', {'class':'_3WhJ'})
+    for index in column:
+        raw = index.findAll('div', {'class':'_2i1r', 'style':"width:25%;"})
+        for i in raw:
+            OnedetailsDict = {}
+            # _2i1r = i.find('div', {'class':'_2i1r'})
+            _3WhJ = i.find('div', {'class':'_3WhJ'})
 
-        url = _3WhJ.a['href'] # url
+            url = _3WhJ.a['href'] # url
 
-        name = _3RA.a['title'] # name
+            name = _3RA.a['title'] # name
 
-        _3nWP = _3WhJ.find('div', {'class':'_3nWP'})
-        imageUrl = _3nWP.img['src'] # image url
+            _3nWP = _3WhJ.find('div', {'class':'_3nWP'})
+            imageUrl = _3nWP.img['src'] # image url
 
-        _2bo3 = _3WhJ.find('div', {'class':'_2bo3'})
-        _1kMS = _2bo3.find('div', {'class':'_1kMS'})
-        prise = _1kMS.span.text # prise
+            _2bo3 = _3WhJ.find('div', {'class':'_2bo3'})
+            _1kMS = _2bo3.find('div', {'class':'_1kMS'})
+            prise = _1kMS.span.text # prise
 
-        OnedetailsDict["pickleUrl"] = url
-        OnedetailsDict["Name"] = name
-        OnedetailsDict["ImageUrl"] = imageUrl
-        OnedetailsDict["peise"] = prise
+            OnedetailsDict["number"] = num
+            OnedetailsDict["pickleUrl"] = url
+            OnedetailsDict["Name"] = name
+            OnedetailsDict["ImageUrl"] = imageUrl
+            OnedetailsDict["peise"] = prise
 
-        AlldetailsList.append(OnedetailsDict)
+            AlldetailsList.append(OnedetailsDict)
+            
+            num = num + 1
     return AlldetailsList
 
 topDetails = topDetailsOfPickles(url)
